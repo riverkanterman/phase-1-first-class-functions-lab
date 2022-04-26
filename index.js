b42e3bd6578bfba2e78ac7b212efb0691525d010
River <+>
2022-04-26 00:30:08 -0400

    commit tree c61c6b8823c55e1e586801783254257add020f4c

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Claudia']
+
+const returnFirstTwoDrivers = drivers => [drivers[0],drivers[1]]
+const returnLastTwoDrivers = drivers => [drivers[2],drivers[3]]
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = (x) => {
+    return function(fare) {return x * fare}
+};
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = (drivers, returnFirstTwoDrivers) => {
+    return returnFirstTwoDrivers(drivers)
+}
